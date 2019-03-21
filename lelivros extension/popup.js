f3332fcf5181df7181799fb08af9910ac113377a
Brayan Fernandes <brayanfernandesmates@gmail.com>
2019-03-20 23:17:27 -0300

    minor change

--- lelivros extension/popup.js
+++ lelivros extension/popup.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let startButton = document.getElementsByClassName("start downloading")[0].onclick = function(){
+        
+    };
 
 }, false);
