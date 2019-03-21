bf0a29a57557f656ed43a8d7b7e48d3a00ebdb97
Brayan Fernandes <brayanfernandesmates@gmail.com>
2019-03-20 23:39:32 -0300

    minor change

--- lelivros extension/popup.js
+++ lelivros extension/popup.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function () {
         if(this.innerText == "Start"){
             this.innerText = "Stop";
             bg.buttonState = true;
-            console.log(bg.buttonState);
+            
         }
         else{
             this.innerText = "Start";
             bg.buttonState = false;
-            console.log(bg.buttonState);
+            
         }
     };
 
